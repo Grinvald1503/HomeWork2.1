2077270b25354a18bdf0c499ed92091cbcdead1b
Aleksandr <aleksanches123@mail.ru>
2022-12-26 20:44:05 +0300

    Задание 1

---
+++ src/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private String color;
+    private int year;
+    private String country;
+
+    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+}
